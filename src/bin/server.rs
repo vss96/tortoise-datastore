be6042e8ae27b3bfc8111d19f35db96d454c95d5
Vikas S Shetty <shettyvikas209@gmail.com>
2022-02-28 16:41:41 +0530

    Add openssl to enable http2

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,7 @@
 use std::env::current_dir;
 
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use tortoise_datastore::{get_probe, update_probe, LsmEngine};
 use tracing::info;
 use tracing_subscriber;
@@ -14,11 +15,16 @@ async fn hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
     info!("Starting server on port 8088");
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
     let engine = LsmEngine::open(current_dir()?).unwrap();
 
     HttpServer::new(move || {
         App::new()
-            .data(engine.clone())
+            .app_data(engine.clone())
             .service(hello)
             .service(update_probe)
             .service(get_probe)
